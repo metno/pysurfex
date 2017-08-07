a4acfb397fdf4623ae1f2c5ddc134c224559f0bf
Trygve Aspelien <trygveasp@met.no>
2017-08-07 15:42:38 +0200

    Implemented reading from ASCII history files

--- forcing/inputFromSurfex.py
+++ forcing/inputFromSurfex.py
@@ -3,20 +3,179 @@ import numpy as np
 from datetime import datetime,timedelta
 from netCDF4 import Dataset
 from forcing.timeSeries import TimeSeries
+from forcing.surfexGeo import IGN
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcl
+import types
 
-class InputFromSurfex(TimeSeries):
+class SurfFile(object):
+
+    def __init__(self):
+        self.geo=None
+        #print "Constructed SurfFile"
+
+    def one2two(self,field):
+        if self.geo.nx < 0 or self.geo.ny < 0:
+            print "The dimensions are not initialized: ",self.geo.nx,self.geo.ny
+            sys.exit(1)
+        fieldall = np.zeros(self.geo.nx * self.geo.ny)
+        fieldall.fill(np.nan)
+        if self.geo.mask == None:
+            print "Assuming uniform mask"
+        else:
+            if len(self.geo.mask) != len(field): print "ERROR: Rank mismatch: ", len(self.geo.mask), len(field); sys.exit(1)
+            for i in range(0, len(field)): fieldall[self.geo.mask[i]] = field[i]
+
+        field2d = np.reshape(fieldall, [self.geo.ny, self.geo.nx])
+        field2d[field2d == 1e+20] = np.nan
+        return field2d
+
+    def plot_field(self,field2d,title=None,intervals=20,bd=5000,zero=False,cmap_name=None):
+
+        if self.geo.X is None or self.geo.Y is None:
+            print "Object does not have X and Y defined!"
+            sys.exit(1)
+
+        X=self.geo.X
+        Y=self.geo.Y
+        nx=self.geo.nx
+        ny=self.geo.ny
+        proj=self.geo.proj
+
+        if isinstance(field2d,list):
+            print "Converting list to 2D numpy array"
+            field2d=self.one2two(field2d)
+
+        ax = plt.axes(projection=proj)
+        ax.set_global()
+        ax.coastlines(resolution="10m")
+
+        ax.set_extent([X[0, 0] - bd, X[ny - 1, nx - 1] + bd, Y[0, 0] - bd,
+                       Y[ny - 1, nx - 1] + bd], proj)
+
+        if not zero: field2d[field2d == 0. ] =np.nan
+
+        min_value = float(np.nanmin(field2d))
+        max_value = float(np.nanmax(field2d))
+
+        #print min_value, max_value, intervals
+        limits = np.arange(min_value,max_value, (max_value - min_value) / float(intervals), dtype=float)
+        #print limits
+
+        if cmap_name is None:
+            cmap = plt.get_cmap('Purples')
+        else:
+            cmap = plt.get_cmap(cmap_name)
+
+        if title is not None:
+            plt.title(title)
+
+        plt.imshow(field2d, extent=(X.min(),X.max(),Y.max(),Y.min()),
+                   transform=proj, interpolation="nearest", cmap=cmap)
+
+        def fmt(x, y):
+            i = int((x - X[0, 0]) / 2500.)
+            j = int((y - Y[0, 0]) / 2500.)
+
+            # print x,y,lon,lat,lon0,lat0,i,j,zs2d.shape[0],zs2d.shape[1]
+            z = np.nan
+            if i >= 0 and i < field2d.shape[1] and j >= 0 and j < field2d.shape[0]:  z = field2d[j, i]
+            return 'x={x:.5f}  y={y:.5f}  z={z:.5f}'.format(x=i, y=j, z=z)
+
+        ax.format_coord = fmt
+        plt.clim([min_value, max_value])
+        norm = mcl.Normalize(min_value, max_value)
+        sm = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+        sm._A = []
+        cb = plt.colorbar(sm, ticks=limits)
+        cb.set_clim([min_value, max_value])
+        plt.show()
+
+class AsciiSurfFile(SurfFile):
+
+    def __init__(self,fname):
+        super(AsciiSurfFile, self).__init__()
+        self.fname=fname
+        grid = self.read_field("&FULL", "GRID_TYPE", type="string")
+        if grid[0] == "IGN":
+            lambert = self.read_field("&FULL", "LAMBERT", type="integer")[0]
+            xx = self.read_field("&FULL", "XX")
+            xdx = self.read_field("&FULL", "DX")
+            yy = self.read_field("&FULL", "XY")
+            xdy = self.read_field("&FULL", "DY")
+            self.geo = IGN(lambert, xx, yy, xdx, xdy)
+        else:
+            print "Grid " + str(grid[0]) + " not implemented!"
+            exit()
+
+
+    def read_field(self,read_tile,read_par,type="float",patches=1):
+        # Add & if not given
+        if read_tile.find('&') < 0: read_tile='&'+read_tile
+        file = open(self.fname,mode="r")
+        read_desc=False
+        read_value=False
+        values=[]
+        for line in file:
+            #print str(i)+":T:"+line
+            words=line.split()
+            if len(words) > 0:
+                if read_value and not read_desc:
+                    if words[0].find('&') < 0:
+                        #print "Value:", line
+                        try:
+                            if type.lower() == "float":
+                                for i in range(0, len(words)):
+                                    val=float(words[i].replace("D", "E"))
+                                    if val == 1e+20: val=np.nan
+                                    values.append(val)
+                            elif type.lower() == "string":
+                                for i in range(0, len(words)):
+                                    values.append(words[i])
+                            elif type.lower() == "integer" or  type.lower() == "int":
+                                for i in range(0, len(words)):
+                                    values.append(int(words[i]))
+                            else:
+                                print "Type not implemented ",type
+                                exit(1)
+                        except ValueError:
+                            print 'Conversion from '+str(words)+" to "+type+" does not work!"
+                            print "Try a different type!"
+                            sys.exit(1)
+
+                if read_desc:
+                    #print "Description: ", words[0]
+                    read_desc = False
+                    read_value = True
+
+                if words[0].find('&') >= 0:
+                    tile=words[0]
+                    par=words[1]
+
+                    read_value=False
+                    if tile.strip().lower() == read_tile.lower() and par.lower() == read_par.lower():
+                        read_desc=True
+                        read_value=False
+                        #print "Found:", tile,par
+
+        if len(values) == 0: print "No values found!"
+        return values
+
+
+class TimeSeriesInputFromSurfex(TimeSeries):
      """ 
      Reading surfex output
      """
 
      def __init__(self,base_time="NA",npoints=1):
-       super(InputFromSurfex, self).__init__()
+       super(TimeSeriesInputFromSurfex, self).__init__()
        self.base_time=base_time
        self.npoints=npoints
        self.my_data=list()
 
 
-class ReadFromASCIIFile(InputFromSurfex):
+class ReadFromASCIIFile(TimeSeriesInputFromSurfex):
      """
      Read from ASCII file
      """
@@ -56,7 +215,7 @@ class ReadFromASCIIFile(InputFromSurfex):
            self.values.append(val)
 
 
-class NetCDF(InputFromSurfex):
+class NetCDF(TimeSeriesInputFromSurfex):
 
      """
      Reading surfex NetCDF output 
@@ -76,7 +235,7 @@ class NetCDF(InputFromSurfex):
        fh.close()
 
 
-class Texte(InputFromSurfex):
+class Texte(TimeSeriesInputFromSurfex):
      """
      Reading surfex TEXTE output
      """
---
+++ forcing/surfexGeo.py
@@ -0,0 +1,150 @@
+import numpy as np
+import cartopy.crs as ccrs
+import os
+
+class SurfexGeo(object):
+
+    def __init__(self):
+        self.X=None
+        self.Y=None
+        self.mask=None
+        self.nx=-1
+        self.ny=-1
+        #print "Contructed SurfexGeo"
+
+class IGN(SurfexGeo):
+
+    def __init__(self,lambert,xx,yy,xdx,xdy,recreate=False):
+
+        super(IGN, self).__init__()
+
+        if lambert == 7:
+            self.proj = ccrs.LambertConformal(central_longitude=15., central_latitude=63., false_easting=922442.1875,
+                                         false_northing=1129321.75, standard_parallels=[63.])
+        else:
+            print "Lambert not defined:",lambert
+            exit(1)
+
+        self.pxall=self.get_coord(xx,xdx,"x",recreate)
+        self.pyall=self.get_coord(yy,xdy,"y",recreate)
+        self.mask=self.ign_mask(self.pxall,self.pyall,xx,yy,recreate)
+        self.nx=len(self.pxall)
+        self.ny=len(self.pyall)
+
+        self.X, self.Y = np.meshgrid(self.pxall, self.pyall)
+        self.lons,self.lats = np.meshgrid(self.pxall, self.pyall)
+
+        g0 = ccrs.Geodetic()
+        self.lons=[]
+        self.lats=[]
+        for i in range(0, len(self.pxall)):
+            for j in range(0, len(self.pyall)):
+                self.X[j, i] = self.pxall[i]
+                self.Y[j, i] = self.pyall[j]
+                lon,lat=g0.transform_point(self.pxall[i],self.pyall[j],self.proj)
+                self.lons.append(lon)
+                self.lats.append(lat)
+
+    def get_coord(self,pin,pdin,coord,recreate=False):
+
+        pout=[]
+        cache="/tmp/."+coord+"_cached"
+        if os.path.isfile(cache) and not recreate:
+             f = open(cache)
+             cached_coord = f.read().splitlines()
+             f.close()
+             for i in range(0,len(cached_coord)):
+                  pout.append(float(cached_coord[i]))
+             return pout
+
+        zdout=[]
+        ksize=0
+        if len(pin) > 0:
+            zdout.append(float(pdin[0])/2.)
+            pout.append(pin[0])
+            ksize=1
+            if len(pin) > 1:
+                ksize=2
+                pout.append(pin[0]-pdin[0])
+                zdout.append(0.)
+            if len(pin) > 2:
+                ksize = 3
+                pout.append(pin[0] + pdin[0])
+                zdout.append(0.)
+
+        #print ksize
+        for i in range(0,len(pin)):
+            for j in range(0,ksize):
+                #print i,j,len(pin),ksize,pout[j],pin[i]
+                if pout[j] == pin[i]: break
+                if j == ksize-1:
+                    ksize=ksize+1
+                    pout.append(pin[i])
+                    zdout.append(float(pdin[i])/2.)
+
+            # Mesh constrains
+            for j in range(0,ksize):
+                #print i, j, len(pin), ksize, pout[j], pin[i]
+                if pout[j] < pin[i] and (pout[j]+zdout[j])>=(pin[i]-pdin[i]): break
+                if j == ksize-1:
+                    ksize=ksize+1
+                    pout.append(pin[i]-pdin[i])
+                    zdout.append(0.)
+
+            for j in range(0,ksize):
+                if pout[j] > pin[i] and (pout[j]-zdout[j])<=(pin[i]+pdin[i]): break
+                if j == ksize-1:
+                    ksize=ksize+1
+                    pout.append(pin[i]+pdin[i])
+                    zdout.append(0.)
+
+        # Sort pout
+        pout=sorted(pout)
+
+        f = open(cache, "w")
+        for i in range(0, len(pout)):
+            f.write(str(pout[i]) + "\n")
+        print "Cached coordinates for : ",coord
+        f.close()
+
+        return pout
+
+    def ign_mask(self,pxall,pyall,xx,yy,recreate):
+        mask=[]
+
+        cache="/tmp/.mask"
+        if os.path.isfile(cache) and not recreate:
+            f = open(cache)
+            cached_mask = f.read().splitlines()
+            f.close()
+            for i in range(0,len(cached_mask)):
+                mask.append(int(cached_mask[i]))
+
+            if len(mask) != len(xx) or len(mask) != len(yy): print "Cached mask mismatch! ",len(mask),len(xx),len(yy); exit(1)
+            return mask
+
+        print "Creating mask. This takes time:"
+        l=-1
+        for j in range(0, len(pyall)):
+            for i in range(0,len(pxall)):
+                l=l+1
+                for k in range (0,len(xx)):
+                    if xx[k] == pxall[i] and yy[k] == pyall[j]:
+                        mask.append(l)
+                        break
+
+            print j,"/",len(pyall)
+
+        # Cache mask for later use
+        #if len(mask) != len(xx) or len(mask) != len(yy): print "Mask mismatch! ", len(mask), len(xx), len(yy); exit(1)
+        f=open(cache,"w")
+        for i in range(0,len(mask)):
+            f.write(str(mask[i])+"\n")
+
+        #f.write("mask="+str(mask)+"\n")
+        print "Created mask: ",mask
+        f.close()
+        return mask
+
+
+
