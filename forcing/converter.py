Fix uncorrect winddir converter and alpha calculation

forcing/converter.py
@@ -84,27 +84,7 @@ class Converter:
                 field=np.sqrt(np.square(field_x)+np.square(field_y))
                 np.where(field<0.005,field,0)
             elif self.name == "winddir":
-                windspeed=np.sqrt(np.square(field_x)+np.square(field_y))
-
-                # TODO: Check for correctness and rotation!
-            #    print(alpha.shape)
-                # Special cases
-                field[(field_x == 0)] = 180.
-                field[(field_y == 0) & (field_x > 0)] = 270.
-                field[(field_y == 0) & (field_x < 0)] = 90.
-
-                # If we have y_wind and positive x_wind
-                field = np.where((field_x > 0.) & (field_y != 0), np.add(180., np.divide(
-                np.multiply(np.arccos(np.divide(field_y, windspeed)), 90.), np.arccos(0.))), field)
-                # If we have y_wind and negative x_wind
-                field = np.where((field_x < 0.) & (field_y != 0),
-                    np.divide(np.multiply(np.arccos(np.multiply(-1., np.divide(field_y, windspeed))), 90.),
-                    np.arccos(0.)), field) + alpha
-                # field[(field_x<0.) & (field_y != 0)]=np.divide(np.multiply(np.arccos(np.multiply(-1.,np.divide(field_y,windspeed))),90.),np.arccos(0.))
-                # Set 360. to 0.
-                field[(field == 360.)] = 0.
-                # TODO field = field + alpha ; alpha is grid rotation
-                
+                field = np.mod(np.rad2deg(np.arctan2(field_x,field_y)) + 180 - alpha, 360)
 
         elif self.name == "rh2q":
             field_rh = self.rh.read_variable(geo, validtime,cache) #

forcing/interpolation.py
@@ -36,12 +36,12 @@ def alpha_grid_rot(lon,lat):
     i1 = np.arange(nx-1)
 
     dlon[0:-1,:] = np.sign(lon[i1+1,:]-lon[i1,:])*distance(lon[i1,:],lat[i1,:],lon[i1+1,:],lat[i1,:])
-    dlat[0:-1,:] = distance(lon[i1,:],lat[i1,:],lon[i1,:],lat[i1+1,:])
+    dlat[0:-1,:] = -np.sign(lat[i1+1,:]-lat[i1,:])*distance(lon[i1,:],lat[i1,:],lon[i1,:],lat[i1+1,:])
 
-    dlon[-1,:] = distance(lon[-2,:],lat[-2,:],lon[-1,:],lat[-2,:])
-    dlat[-1,:] = distance(lon[-2,:],lat[-2,:],lon[-2,:],lat[-1,:])
+    dlon[-1,:] = np.sign(lon[-1,:]-lon[-2,:])*distance(lon[-2,:],lat[-2,:],lon[-1,:],lat[-2,:])
+    dlat[-1,:] = -np.sign(lat[-1,:]-lat[-2,:])*distance(lon[-2,:],lat[-2,:],lon[-2,:],lat[-1,:])
 
-    alpha = np.rad2deg(np.arctan2(dlon,dat)) 
+    alpha = np.rad2deg(np.arctan2(dlon,dlat)) 
     return alpha
 
 