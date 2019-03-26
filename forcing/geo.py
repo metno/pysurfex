740e790c2f3d4f9746300f391e812da0a7d81560
Trygve Aspelien <trygveasp@met.no>
2019-03-26 12:54:31 +0100

    Add correct rotation of conf_proj and the option to specify a yaml file with conf_proj_domain and NWP domain settings

--- forcing/geo.py
+++ forcing/geo.py
@@ -58,3 +58,18 @@ class Domain(Geo):
 
         super(Domain,self).__init__(proj,nlons*nlats,nlons,nlats,lons,lats)
 
+class confProjDomain(Geo):
+
+    def __init__(self,lonc,latc,lon0,lat0,gsize,nx,ny):
+
+        from forcing.conf_proj import conf_proj_geo
+
+        lons,lats=conf_proj_geo(lonc,latc,lon0,lat0,gsize,nx,ny)
+
+        # Convert to longitude/latiudes Set the list as surfex expects
+        lons = lons.tolist()
+        lats = lats.tolist()
+
+        proj="+proj=lcc +lat_0="+str(latc)+"+lon_0="+str(lonc)+" +lat_1="+str(lat0)+" +lat_2="+str(lat0)+" +no_defs +R=6.371e+06"
+        super(confProjDomain,self).__init__(proj,nx*ny,nx,ny,lons,lats)
+
