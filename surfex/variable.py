707cbf0bfa3d7d3ee262ed0126977a6461fe5ca2
Trygve Aspelien <trygveasp@met.no>
2021-05-11 08:39:52 +0200

    Bugfix for reading surfex forcing t!=0 and members in netcdf

--- surfex/variable.py
+++ surfex/variable.py
@@ -182,8 +182,9 @@ class Variable(object):
             member = None
             if "member" in self.var_dict:
                 member = self.var_dict["member"]
-                if not isinstance(member, list):
-                    member = [member]
+                if member is not None:
+                    if not isinstance(member, list):
+                        member = [member]
             var = surfex.NetCDFReadVariable(name, level=level, units=units, member=member)
         elif self.var_type == "grib1":
             par = self.var_dict["parameter"]
